0219c2e3033a1178558c2a0ab97062c957270a74
JohnOK <john.ok.esp@gmail.com>
2022-06-05 18:49:05 +0200

    Made all funcs fat arrow funcs. Added function to find all Armstrong numbers up to a given number. Added edge cases to spec tests.

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,28 +1,21 @@
-// How can you make this more scalable and reusable later?
-// questions the program needs to answer: 
-// 1. how many digits? variable for that value is input to the math operation
-// 2. does sum of individual digit values to the power of the # digits = the number itself
-// how to find # of digits:
-
-//use 2 functions
-//1. first checks whether a number is an armstrong number: isArmstrongNumber
-// 2. 2nd runs the first on the array/list: findArmstrongNumbers
-
-exports.findArmstrongNumbers = function(array) {
-    return array.filter(each => isArmstrongNumber(each))
-};
-
-
-function isArmstrongNumber(num){
-    
+isArmstrongNumber = num => {
+    // Make num a string and split it so that each number can be prcoessed individually
     let numString = num.toString()
-    //return numString
-    let numStringArray = numString.split("")
-    let powerNum = numStringArray.length
-    let numStringArray2 = numStringArray.map(x => (+x)**powerNum)
-    let finalSum = numStringArray2.reduce((p,c) => p+c)
+    let digits = numString.split("")
+
+    // Run the Armstrong calculation
+    let exponent = digits.length
+    let raisedDigits = digits.map(digit => (+digit)**exponent)
+    let sumOfRaisedDigits = raisedDigits.reduce((aggregator,raisedDigit) => aggregator+raisedDigit)
     
-    return finalSum === num
+    return sumOfRaisedDigits === num
 }
 
-//console.log(findArmstrongNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 155, 156, 370, 371, 407, 507]))
+exports.findArmstrongNumbers = array => array.filter(element => isArmstrongNumber(element));
+
+findAllArmstrongNumbers = num => {
+    array = Array.apply(null, {length: num}).map(Number.call, Number)
+    return findArmstrongNumbers(array);
+}
+
+// console.log(findAllArmstrongNumbers(9999));
